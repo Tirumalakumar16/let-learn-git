4397c457012a5003f34a36dbcc0fb6badbfb532f
tirumala kumar <kumarktk4169@gmail.com>
2023-04-11 17:07:24 +0530

    added multiply method

---
+++ multiply.java
@@ -0,0 +1,11 @@
+package letlearnGit;
+
+public class multiply {
+    
+    public static void main(String[] args) {
+         int a=33;
+         int b=55;
+
+         System.out.println("This is the product :"+(a*b));
+    }
+}
