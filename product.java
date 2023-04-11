4ad941337598986f6a571e0eafc49fee61ce1ba6
tirumala kumar <kumarktk4169@gmail.com>
2023-04-11 17:04:25 +0530

    add product.java

---
+++ product.java
@@ -0,0 +1,12 @@
+package letlearnGit;
+public class product {
+    
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=33;
+
+        System.out.println("This is the product :"+a*b);
+
+    }
+}
